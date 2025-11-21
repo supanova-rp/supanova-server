61c468bcb636f97b9d4375ee3aceb273eb63a3db
Jamie Garner <jamiegarner123@gmail.com>
2025-11-22 10:45:56 +1100

    test change

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -1,9 +1,9 @@
 package domain
 
 type SystemRepository interface {
-	// TODO: add the method signature here that is needed by the handler in handlers/course.go
+	// TODO: add the method signature here that is needed by the handler in handlers/course.go.
 }
 
 type CourseRepository interface {
-	// TODO: add the method signature here that is needed by the handler in handlers/course.go
+	// TODO: add the method signature here that is needed by the handler in handlers/course.go.
 }
