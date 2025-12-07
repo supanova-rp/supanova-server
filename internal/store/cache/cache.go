b62676fab2657cf271e242fc8c8a7ee923eda403
Marie20767 <marie20767@gmail.com>
2025-12-07 11:16:50 +1100

    Added cache data structure in store

---
+++ internal/store/cache/cache.go
@@ -0,0 +1,56 @@
+package cache
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrCacheMiss               = errors.New("cache miss")
+	ErrCacheEntryAlreadyExists = errors.New("entry with this key already exists in cache")
+)
+
+type Cache[T any] struct {
+	keys     []uuid.UUID
+	itemsMap map[uuid.UUID]T
+}
+
+func New[T any](data T) *Cache[T] {
+	return &Cache[T]{
+		keys:     []uuid.UUID{},
+		itemsMap: map[uuid.UUID]T{},
+	}
+}
+
+func (c *Cache[T]) Set(key uuid.UUID, value T) error {
+	if _, exists := c.itemsMap[key]; exists {
+		return ErrCacheEntryAlreadyExists
+	}
+
+	c.keys = append(c.keys, key)
+	c.itemsMap[key] = value
+
+	return nil
+}
+
+func (c *Cache[T]) Get(key uuid.UUID) (T, error) {
+	value, exists := c.itemsMap[key]
+
+	if !exists {
+		return value, ErrCacheMiss
+	}
+
+	return value, nil
+}
+
+func (c *Cache[T]) Remove(key uuid.UUID) {
+	for i, cKey := range c.keys {
+		if cKey == key {
+			c.keys = append(c.keys[:i], c.keys[i+1:]...)
+			break
+		}
+	}
+
+	delete(c.itemsMap, key)
+}
