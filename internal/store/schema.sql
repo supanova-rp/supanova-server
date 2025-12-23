e41197d3b85178a5e248b72bbd7ed638add0e778
Marie20767 <marie20767@gmail.com>
2025-12-23 14:22:16 +1100

    Changed email_name and template_name to TEXT

--- internal/store/schema.sql
+++ internal/store/schema.sql
@@ -99,8 +99,8 @@ CREATE TABLE email_failures (
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   error TEXT NOT NULL,
-  template_name VARCHAR(100) NOT NULL,
+  template_name TEXT NOT NULL,
   template_params JSONB NOT NULL DEFAULT '{}'::jsonb,
-  email_name VARCHAR(100) NOT NULL,
+  email_name TEXT NOT NULL,
   retries INT NOT NULL DEFAULT 5 
 );
